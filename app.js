67232c1475c2ee67c479f28c39947b7278156a94
HAFIZ MUHAMMAD OWAIS QARNI <www.owais664@gmail.com>
2024-10-19 20:02:42 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+var tom=[
+{ Name:'owais',contact:"888776655",addres:"malir"},
+ {Name:'ahmad',contact:"8887765543",addres:"karachi"},
+{ Name:'hasnain',contact:"888776699",addres:"islamabad"},
+{ Name:'umaim',contact:"88877644444",addres:"lohore"}
+]
+for (let i = 0; i < tom.length; i++) {
+    console.log  (tom[i].Name)
+    console.log  (tom[i].contact)
+    console.log  (tom[i].addres)
+
+    
+}
+
